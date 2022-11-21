fd11c1db8e658b091b426740d1ab5c2ce65b70f3
Evgeniy_Vladimir <6484588@mail.ru>
2022-11-21 23:32:32 +0300

    java_lec1

--- java_lec1/program.java
+++ java_lec1/program.java
@@ -601,20 +601,20 @@ package java_lec1;
 //     } }
 
 // Работа с файлами. Чтение, Вариант посимвольно
-import java.io.*;
-public class Program {
-    public static void main(String[] args) throws Exception {
-        FileReader fr = new FileReader("file.txt");
-        int c;
-        while ((c = fr.read()) != -1) {
-            char ch = (char) c;
-            if (ch == '\n') {
-                System.out.print(ch);
-            } else {
-                System.out.print(ch);
-            }
-        }
-    } }
+// import java.io.*;
+// public class Program {
+//     public static void main(String[] args) throws Exception {
+//         FileReader fr = new FileReader("file.txt");
+//         int c;
+//         while ((c = fr.read()) != -1) {
+//             char ch = (char) c;
+//             if (ch == '\n') {
+//                 System.out.print(ch);
+//             } else {
+//                 System.out.print(ch);
+//             }
+//         }
+//     } }
 
 // Работа с файлами. Чтение, Вариант построчно
 //     import java.io.*;
